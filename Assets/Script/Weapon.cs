c1307b7e01b6f28d8ff70c3b22ecab3694392e91
nafis-croscheck <nafis.zaman1.an@gmail.com>
2026-04-09 18:11:43 +0600

    probuilder

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using StarterAssets;
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] GameObject hitVFXPrefab;
+    [SerializeField] Animator animator;
+    const string shootAnimation = "shoot";
     [SerializeField] private int damage = 10;
     [SerializeField] ParticleSystem muzzleFlash;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +20,8 @@ public class Weapon : MonoBehaviour
         if(starterAssetsInputs.shoot)
         {
             muzzleFlash.Play();
+            animator.Play(shootAnimation, 0, 0f);
+            starterAssetsInputs.ShootInput(false);
             Shoot();
         }
     }
@@ -26,10 +31,10 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
+            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
             Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * Mathf.Infinity, Color.red);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damage);
-            starterAssetsInputs.ShootInput(false);
         }
     }
 }
